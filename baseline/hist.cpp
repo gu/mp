652e172d1c4efc179961a9c75d778d76ed87758b
Freddy Gu <freddy.gu@gmail.com>
2018-04-02 20:30:22 -0400

    Added histogram creation for baseline

--- baseline/hist.cpp
+++ baseline/hist.cpp
@@ -8,10 +8,13 @@
 
 using namespace std;
 
-void readBmp(char* filename, vector<vector<int>> &mat, vector<vector<int>> &bin_mat);
+void readBmp(char* filename, vector<vector<int>> &mat, vector<vector<int>> &bin_mat, vector<vector<int>> &sat);
 void generateBoundaries(int bins, vector<pair<int, int>> &boundaries);
 void generateBinMat(int bin, vector<pair<int,int>> boundaries, 
                     vector<vector<int>> &mat, vector<vector<int>> &bin_mat);
+void generate_sat(vector<vector<int>>& mat, vector<vector<int>>& sat);
+bool validate_roi(int r1, int r2, int c1, int c2, int rows, int cols);
+int sum_roi(int r1, int r2, int c1, int c2, vector<vector<int>>& sat);
 
 int main(int argc, char* argv[]) {
     // suppress warnings
@@ -19,8 +22,10 @@ int main(int argc, char* argv[]) {
 
     int bins;
     vector<vector<int>> mat;
+    vector<vector<int>> sat;
     vector<vector<int>> bin_mat;
     vector<pair<int, int>> boundaries;
+    vector<int> hist;
 
     // Check for valid argument input.
     if (argc != 3) {
@@ -36,22 +41,39 @@ int main(int argc, char* argv[]) {
     ss.clear();
 
     generateBoundaries(bins, boundaries);
-    readBmp(argv[1], mat, bin_mat);
+    hist.resize(bins);
+    readBmp(argv[1], mat, bin_mat, sat);
 
-    cout << mat[0][0] << endl;
-    cout << mat[256][256] << endl;
-    cout << mat[511][511] << endl;
+    int r1, r2, c1, c2;
+
+    cout << "Enter coordinates (r1, c1, r2, c2) for ROI: ";
+    cin >> r1 >> c1 >> r2 >> c2;
+    while (!validate_roi(r1, r2, c1, c2, mat.size(), mat[0].size())) {
+        // cout << "Sum of ROI is: " << sum_roi(r1, r2, c1, c2, sat) << endl;
+        cout << "Unvalid coordinates." << endl;
+        cout << "Enter coordinates (r1, c1, r2, c2) for ROI: ";
+        cin >> r1 >> c1 >> r2 >> c2;
+    }
+
+    // cout << mat[0][0] << endl;
+    // cout << mat[256][256] << endl;
+    // cout << mat[511][511] << endl;
 
     // for (int i = 0; i < bins; i++) {
     //     cout << boundaries[i].first << " " << boundaries[i].second << endl;
     // }
 
-    int bin = 0;
-    generateBinMat(bin, boundaries, mat, bin_mat);
+    for (int bin = 0; bin < bins; bin++) {
+        generateBinMat(bin, boundaries, mat, bin_mat);
+        generate_sat(bin_mat, sat);
+        int sum = sum_roi(r1, r2, c1, c2, sat);
+        hist[bin] = sum;
+    }
 
-    cout << bin_mat[0][0] << endl;
-    cout << bin_mat[256][256] << endl;
-    cout << bin_mat[511][511] << endl;
+    for (vector<int>::size_type i = 0; i < hist.size(); i++) {
+        cout << hist[i] << " ";
+    }
+
 
     return 0;
 }
@@ -88,7 +110,7 @@ void generateBoundaries(int bins, vector<pair<int, int>> &boundaries) {
     }
 }
 
-void readBmp(char* filename, vector<vector<int>> &mat, vector<vector<int>> &bin_mat) {
+void readBmp(char* filename, vector<vector<int>> &mat, vector<vector<int>> &bin_mat, vector<vector<int>> &sat) {
     
     FILE* f = fopen(filename, "rb");
     unsigned char info[54];
@@ -104,6 +126,7 @@ void readBmp(char* filename, vector<vector<int>> &mat, vector<vector<int>> &bin_
 
     mat.resize(height, vector<int>(width));
     bin_mat.resize(height, vector<int>(width));
+    sat.resize(height, vector<int>(width));
 
     vector<vector<int>>::iterator r;
     vector<int>::iterator c;
@@ -114,4 +137,40 @@ void readBmp(char* filename, vector<vector<int>> &mat, vector<vector<int>> &bin_
         }
     }
 
+}
+
+int sum_roi(int r1, int r2, int c1, int c2, vector<vector<int>>& sat) {
+    int w, x, y, z;
+    
+    w = r1 == 0 || c1 == 0 ? 0 : sat[r1-1][c1-1];
+    x = r1 == 0 ? 0 : sat[r1-1][c2];
+    y = c1 == 0 ? 0 : sat[r2][c1-1];
+    z = sat[r2][c2];
+
+    return w + z - x - y;
+}
+
+bool validate_roi(int r1, int r2, int c1, int c2, int rows, int cols) {
+    int ret = true;
+    if (r1 < 0 || r1 >= rows) ret = false;
+    if (r2 < 0 || r2 >= rows) ret = false;
+    if (c1 < 0 || c1 >= cols) ret = false;
+    if (c2 < 0 || c2 >= cols) ret = false;
+    return ret;
+}
+
+void generate_sat(vector<vector<int>>& mat, vector<vector<int>>& sat) {
+    int rows = (int) mat.size();
+    int cols = (int) mat[0].size();
+
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            int w, x, y, z;
+            w = r == 0 || c == 0 ? 0 : sat[r-1][c-1];
+            x = r == 0 ? 0 : sat[r-1][c];
+            y = c == 0 ? 0 : sat[r][c-1];
+            z = mat[r][c];
+            sat[r][c] = (x + y) - w + z;
+        }
+    }
 }
